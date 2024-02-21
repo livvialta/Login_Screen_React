290100c490881c0cff53eca747b81218f8921d4f
Livia Vialta Peretta Bazilio <LBazilio@slb.com>
2024-02-21 14:27:58 -0300

    login screen - front end in react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="login-form-wrap">
+          <img src='https://mentorprofissional.com.br/wp-content/uploads/2018/12/senai-1.png' width="300" height="100"></img>
+
+          <form className='login-form'>
+            <input type='email' name='email' placeholder='Email' required/>
+            <input type='password' name='password' placeholder='Senha' required/>
+
+            <button type='submit' className='btn-login'>Entrar</button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
